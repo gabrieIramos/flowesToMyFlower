a474e4cf91dfa61621f6c4d5de9e69ec6c327ad5
Gabriel Ramos <gabrielrcoca@outlook.com>
2024-08-25 01:22:20 -0300

    fix music

--- java.js
+++ java.js
@@ -1,12 +1,14 @@
-onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
-    var music = document.getElementById("myAudio");
-    music.volume = 0.2;
-    music.currentTime = 50;
-    music.play();
 
-    
+function Load() {
+    var button = document.getElementById('Init');
+    button.style.display = 'none';
+    const c = setTimeout(() => {
+        document.body.classList.remove("not-loaded");
+        var music = document.getElementById("myAudio");
+        console.log(music);
+        music.currentTime = 50;
+        music.play();
+        music.volume = 0.2;
+        clearTimeout(c);
+    }, 1000);
+};
